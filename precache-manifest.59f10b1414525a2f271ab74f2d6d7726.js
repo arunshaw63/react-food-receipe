5675deb9a61f2941747242b197536060715f68ca
arunshaw <arunshaw63@gmail.com>
2021-06-20 11:30:17 +0530

    Updates

---
+++ precache-manifest.59f10b1414525a2f271ab74f2d6d7726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1df7e113432c7f729b1861460af523",
+    "url": "/react-food-receipe/index.html"
+  },
+  {
+    "revision": "a198fa1072e85c1245fc",
+    "url": "/react-food-receipe/static/css/main.792c9fdc.chunk.css"
+  },
+  {
+    "revision": "a3c68ad5ffe685cf975c",
+    "url": "/react-food-receipe/static/js/2.52f2abd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-food-receipe/static/js/2.52f2abd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a198fa1072e85c1245fc",
+    "url": "/react-food-receipe/static/js/main.cf38cda6.chunk.js"
+  },
+  {
+    "revision": "85f58d1cb00409c16757",
+    "url": "/react-food-receipe/static/js/runtime-main.630143c6.js"
+  }
+]);
